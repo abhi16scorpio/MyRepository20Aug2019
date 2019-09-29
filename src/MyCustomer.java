50a9870596f3f050501e35688852a4399c648abe
Abhishek <Abhishek@Abhishek-PC>
2019-09-29 14:57:39 +0530

    This is my second commit

---
+++ src/MyCustomer.java
@@ -0,0 +1,48 @@
+
+public class MyCustomer 
+{
+	
+	private int age;
+	private String name;
+	private String city;
+	
+	public int getAge() 
+	{
+		return age;
+	}
+	
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	
+	public String getCity() 
+	{
+		return city;
+	}
+	
+	public void setCity(String city) 
+	{
+		this.city = city;
+	}
+
+	@Override
+	public String toString() 
+	{
+		return "MyCustomer [age=" + age + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+	
+	
+}
